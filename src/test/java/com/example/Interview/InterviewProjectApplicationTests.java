77f2d320be8280c3b0a9c98e3d7d4c6bc47fe5dd
Abilash Shankarpalli <abhilashabhi0717@gmail.com>
2025-12-15 22:05:57 +0530

    initial commit

---
+++ src/test/java/com/example/Interview/InterviewProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Interview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterviewProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
